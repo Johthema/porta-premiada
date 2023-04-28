bc035a52021f0ef132cf8783aaba5ef4b5534c8c
Johthema <mauriciojodi@gmail.com.br>
2023-04-28 15:37:51 -0400

    Atualiza versao portas selecionadas

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,17 @@
 import { useState } from 'react'
 import Porta from "../components/Porta"
-import { criarPortas } from '../functions/portas'
-//import PortaModel from "../model/porta";
+import { atualizarPortas, criarPortas } from '../functions/portas'
+import PortaModel from "../model/porta";
 
 export default function Home() {
-  // const p1 = new PortaModel(1)
-  // const p2 = new PortaModel(2)
-  // const p3 = new PortaModel(3, false, true)
-  //ou melhor..
-
-  // const [p1, setP1] = useState(new PortaModel(1))
+ 
   const [portas, setPortas] = useState(criarPortas(3, 2))
  
 
   function renderizarPortas(){
     return portas.map(porta=>{
-      return <Porta key={porta.numero} value={porta} onChange={console.log}/>
+      return <Porta key={porta.numero} value={porta} 
+      onChange={ novaPorta=> setPortas(atualizarPortas(portas, novaPorta))}/>
     })
   }
 
