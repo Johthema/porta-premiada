df92b1e92b3197a85a14fb3242667d9351031dd6
Johthema <mauriciojodi@gmail.com.br>
2023-05-01 14:47:09 -0400

    Versao pagina inicial em construção

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import Porta from "../components/Porta"
 import { atualizarPortas, criarPortas } from '../functions/portas'
 import PortaModel from "../model/porta";
-
+import Link from 'next/link'
 export default function Home() {
 
 
   return (
-  <div style={{display: "flex"}}>
-  <h1>Porta premiada</h1>
+  <div >
+  <h1>Porta premiada - Página em construção</h1>
+  <br></br>
+  <Link href="/jogo">
+  <h1 >Iniciar jogo</h1>
+  </Link>
+  
   </div>
   )
 }
